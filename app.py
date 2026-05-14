3619fe9b030ed4dafa37f3e5bfe4f42838a9015d
ramzan0030 <ramzanahmed0030@gmail.com>
2026-05-14 10:52:55 +0200

    opdateret app

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/booking")
+def booking():
+    return "Booking side kommer snart"
+
+@app.route("/login")
+def login():
+    return "Login side kommer snart"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
+    
